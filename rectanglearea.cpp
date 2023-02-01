afd60df13643b16c2efc97130c7d2c20e00f95b6
amd126 <123202049+amd126@users.noreply.github.com>
2023-02-01 13:44:56 -0500

    Update rectanglearea.cpp

--- rectanglearea.cpp
+++ rectanglearea.cpp
@@ -1,4 +1,4 @@
-// This program will calculate the area of a rectangle with the user given measurements
+// This program will calculate the area and perimeter of a rectangle with the user given measurements
 // before printing the values. The user will be prompted to enter
 // the length and width of the rectangle.
 // Aidenn Diaz
@@ -9,6 +9,7 @@ int main()
 float length;
 float width;
 float area;
+float perimeter;
 // Prompt user to enter length.
 cout << "Enter the length of the rectangle" << endl;
 cout << "Then hit enter" << endl;
@@ -19,8 +20,9 @@ cout << "Then hit enter" << endl;
 cin >> width;
 // calc
 area = length * width;
+perimeter = (2 * length) + (2 * width);
 // Output the values.
-cout << "After calculating, the area of the rectangle is "
-<< area;
+cout <<"The length of the rectangle is "<<length<<" \nThe width of the rectangle is "<<width<< "\nAfter calculating, the area of the rectangle is "
+<< area<<"\nand the perimeter of the rectangle is "<<perimeter;
 return 0;
 }
