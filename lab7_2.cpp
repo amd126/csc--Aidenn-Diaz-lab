0e945c122f32977fb431559b1d02865373f822bd
amd126 <123202049+amd126@users.noreply.github.com>
2023-03-08 15:11:19 -0500

    Create lab7_2.cpp

---
+++ lab7_2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+for(int x=9; x<=9 && x>=0; --x)
+  {
+  for(int y=0; y<=x; ++y)
+    {
+    cout << "*";
+    }
+  cout << endl;
+  }
+  return 0;
+  }
